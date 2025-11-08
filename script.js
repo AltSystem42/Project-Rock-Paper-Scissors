f2629bad132b503024b3bd19433eece5a038503a
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2025-11-07 22:01:43 -0400

    First commit of branch
    added queryselector for buttons
    added results to appear below selection
    added scoreboard not functional yet

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
+const playerSelections = document.querySelectorAll(".choice");
+const result = document.getElementById("result")
+const roundCount = document.createElement("div")
+const winCount = document.createElement("div")
+const lostCount = document.createElement("div")
 
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
 
+playerSelections.forEach(choice => {
+  choice.addEventListener("click", function() {
+    const winner = playRound(choice.id);
+    if (winner === "human"){
+        humanScore++;
+    } else if (winner === "pc") {
+        computerScore++;
+    }
+    console.log(round)
+    roundCount.textContent = round;
+    round++; 
+  });
+});
 
-// Play 5 rounds of rock paper scissors
 function playGame (){
     // Declare players score variables
     let humanScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i ++){
-        // Get choices from players
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    let winner = playRound(humanSelection, computerSelection);
-    // will determine who gets a point
-    if (winner === "pc"){
-        computerScore++;
-    } else if (winner == "human"){
-        humanScore++;
-    } 
-    }
+    
     // End of game did you win?
     if (humanScore > computerScore){
-        alert(`You WON! NPC: ${computerScore} You: ${humanScore}`);
+        // alert(`You WON! NPC: ${computerScore} You: ${humanScore}`);
+
     } else if (humanScore < computerScore) {
         alert(`You LOST! NPC: ${computerScore} You: ${humanScore}`)
     } else {
         alert(`It's a TIE NPC:${computerScore} You:${humanScore}`)
     }
-}
-
-//prompt user what their choice will be
-function getHumanChoice (){
-    let choice = prompt("rock, paper, or scissors?").toLowerCase();
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return choice;
-    } else {
-        alert("Choose only 'rock', 'paper', or 'scissors'");
-    };
 }
 
 // Will return the computer's choice of rock, paper, or scissors
@@ -51,30 +51,35 @@ function getComputerChoice() {
     
 }
 //play a round of rock paper scissors
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice()
     if (humanChoice === computerChoice) {
+        result.textContent = `It's a tie (${humanChoice} vs. ${computerChoice})`
         console.log(`It's a tie! ${humanChoice} vs. ${computerChoice}`);
+        return "tie"
     } else if (computerChoice === "rock" && humanChoice === "paper") {
+        result.textContent = `You WIN! Paper Beats Rock`
         console.log(`You WIN! Paper beats Rock`);
         return "human"
     } else if (computerChoice === "paper" && humanChoice == "rock") {
+        result.textContent = `You LOSE! Paper Beats Rock`
         console.log('You LOSE! Paper beats Rock')
         return "pc"
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        result.textContent = `You LOSE! Rock Beats Scissors`
         console.log(`You LOSE! Rock beats Scissors`);
         return "pc"
     } else if (computerChoice === "scissors" && humanChoice == "rock") {
+        result.textContent = `You WIN! Rock Beats Scissors`
         console.log('You WIN! Rock beats Scissors')
         return "human"
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        result.textContent = `You LOSE! Scissors Beats Paper`
         console.log(`You LOSE! Scissors beats Paper`);
         return "pc"
     } else if (computerChoice === "paper" && humanChoice == "scissors") {
+        result.textContent = `You WIN! Scissors Beats Paper`
         console.log('You WIN! Scissors beats Paper')
         return "human"
     }
 }
-
-
-
-playGame();
